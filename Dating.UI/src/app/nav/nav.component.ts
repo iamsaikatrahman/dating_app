2326be20d337f6d9584c77abbb8d3bcecb9c61ca
Saikat Rahman <saikatrahmanofficial@gmail.com>
2023-08-14 23:38:09 +0600

    end of section 6

--- Dating.UI/src/app/nav/nav.component.ts
+++ Dating.UI/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +13,7 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit{
   model: any = {}
 
-  constructor(public accountService: AccountService){}
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService){}
   ngOnInit(): void {
   }
 
@@ -19,14 +21,17 @@ export class NavComponent implements OnInit{
     this.accountService.login(this.model).subscribe({
       next:(response) => {
         console.log(response);
+        this.router.navigateByUrl('/members')
       },
       error:(response) => {
         console.log(response);
+        this.toastr.error(response.error)
       }
     })
   }
 
   logout(){
-    this.accountService.logout()
+    this.accountService.logout();
+    this.router.navigateByUrl('/')
   }
 }
--- Dating.UI/src/app/register/register.component.ts
+++ Dating.UI/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -9,13 +10,17 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private accountService: AccountService){}
+  constructor(private accountService: AccountService, private toastr: ToastrService){}
   register(){
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
-      this.cancel();
-    }, error => {
-      console.log(error);
+    this.accountService.register(this.model).subscribe({
+      next:(respose)=>{
+        console.log(respose)
+        this.cancel();
+      },
+      error:(response) => {
+        console.log(response);
+        this.toastr.error(response.error)
+      }
     })
   }
   cancel(){
